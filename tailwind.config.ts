b679cbc32ad52e4d2be0ac754a44738ed296acb1
David Cubillos <esteban28ec@outlook.com>
2025-01-30 23:23:07 -0500

    add ThemeContext

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				heading: 'var(--heading)',
+				subHeading: 'var(--sub-heading)',
+				backgroundButton: 'var(--background-button)',
+				foregroundButton: 'var(--foreground-button)',
+				cardBackground: 'var(--card-background)',
+				cardHeading: 'var(---card-heading)',
+				cardForeground: 'var(--card-foreground)',
+				cardHighligh: 'var(--card-highlight)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				tertiary: 'var(--tertiary)',
+				links: 'var(--links)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
