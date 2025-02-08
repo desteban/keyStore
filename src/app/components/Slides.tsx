6db8c4273a4ed4521bf448125a8437b40ee8c8b8
David Cubillos <esteban28ec@outlook.com>
2025-02-07 22:50:37 -0500

    Add sample carousel component

--- src/app/components/Slides.tsx
+++ src/app/components/Slides.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Carrousel from '@/components/ui/Carrousel';
-import { CarouselImage } from '@/components/ui/Carrousel/ItemCarrousel';
+import { CarouselImage } from '@/components/ui/Carrousel/CarouselImage';
+import { CarouselItem } from '@/components/ui/Carrousel/ItemCarrousel';
 import image1 from '@assets/1.jpg';
 import image2 from '@assets/2.jpg';
 import image3 from '@assets/3.jpg';
@@ -10,13 +11,29 @@ import image4 from '@assets/4.jpg';
 export default function Slides() {
 	return (
 		<div className="relative px-5">
-			<Carrousel id="offer" interval={6000} className="mt-4">
+			<Carrousel
+				id="offer"
+				interval={3000000}
+				className="mt-4 h-56 md:h-96"
+				scrollType="smooth"
+			>
 				<CarouselImage alt="random" src={image1.src} />
-				<CarouselImage>
+				<CarouselItem>
+					<div className="flex flex-col items-center justify-center h-full">
+						<h5>Hola</h5>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia nulla nam
+							totam ab quidem cumque, nesciunt quibusdam rerum neque dolor sed,
+							perspiciatis adipisci voluptas hic, beatae obcaecati tenetur. Mollitia,
+							illum.
+						</p>
+					</div>
+				</CarouselItem>
+				<CarouselItem>
 					<figure className="w-full h-full">
 						<img alt="random" src={image2.src} className="w-full object-cover" />
 					</figure>
-				</CarouselImage>
+				</CarouselItem>
 				<CarouselImage alt="random" src={image3.src} />
 				<CarouselImage alt="random" src={image4.src} />
 			</Carrousel>
